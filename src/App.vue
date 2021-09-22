df8da29ade9fcb4cd165ae9f73dc6f19ed28334e
Roberto Shimizu <rshimizubr@gmail.com>
2021-09-22 23:22:47 +0000

    refactor: make links navbar and footer flexible

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <v-app>
+    <!-- Navbar -->
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded to="/">Home</v-btn>
-      <v-btn text rounded to="/login">Login</v-btn>
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        :to="link.url"
+      >
+        {{ link.label }}
+      </v-btn>
     </v-app-bar>
     <v-content>
+      <!-- different pages comes here controlled by the router -->
       <router-view></router-view>
     </v-content>
     <!-- Footer -->
@@ -14,13 +25,14 @@
       <v-row justify="center" no-gutters>
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
+          :to="link.url"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
           {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
@@ -42,7 +54,20 @@ export default {
   },
 
   data: () => ({
-    links: ["Home", "Login"],
+    links: [
+      {
+        label: "Home",
+        url: "/",
+      },
+      {
+        label: "Login",
+        url: "/login",
+      },
+      {
+        label: "About",
+        url: "/about",
+      },
+    ],
   }),
 };
 </script>
