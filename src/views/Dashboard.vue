73e321df4cd922313eba4c4267d7fe3d63443fec
Roberto Shimizu <rshimizubr@gmail.com>
2021-09-23 01:06:01 +0000

    feat: add dashboard employees table

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
     <h1>Dashboard</h1>
+    <v-data-table
+      dense
+      :headers="headers"
+      :items="employees"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
   </div>
+  
 </template>
 
 <script>
+// Static Import
+import json from "../data/employees.json";
+
 export default {
   name: "Dashboard",
 
   components: {},
 
-  //   data: () => (),
+  data() {
+    return {
+      headers: [
+        {
+          text: "Id",
+          align: "start",
+          sortable: false,
+          value: "id",
+        },
+        { text: "Name", value: "name" },
+        { text: "Job Title", value: "title" },
+        { text: "Salary", value: "salary" },
+      ],
+      employees: json,
+    };
+  },
 };
 </script>
 
