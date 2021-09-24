7fded39caf620a6b71a6e1845588ecdfb7be4c5f
Roberto Shimizu <rshimizubr@gmail.com>
2021-09-24 11:45:32 +0000

    feat: insert dahsboard components

---
+++ src/components/EmployeesTable.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-data-table
+      dense
+      :headers="headers"
+      :items="employees"
+      :items-per-page="5"
+      class="elevation-1"
+      @click:row="selectRow"
+      :multi-sort="true"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmployeesTable",
+  props: {
+    employees: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      headers: [
+        {
+          text: "Employee ID",
+          align: "start",
+          sortable: false,
+          value: "id",
+        },
+        { text: "Name", value: "name" },
+        { text: "Job Title", value: "title" },
+        { text: "Salary", value: "salary" },
+      ],
+    };
+  },
+  methods: {
+    selectRow(event) {
+      const employee = {
+        name: event.name,
+        title: event.title,
+      };
+      this.$emit("select-employee", employee);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,41 +1,61 @@
 <template>
   <div>
     <h1>Dashboard</h1>
-    <v-data-table
-      dense
-      :headers="headers"
-      :items="employees"
-      :items-per-page="5"
-      class="elevation-1"
-    ></v-data-table>
+    <SalesGraph v-for="sale in sales" :key="`${sale.title}`" :sale="sale" />
+    <StatisticCard
+      v-for="statistic in statistics"
+      :key="`${statistic.title}`"
+      :statistic="statistic"
+    />
+    <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+    <EventTimeline :timeline="timeline" />
+
+    <v-snackbar v-model="snackbar">
+      You have selected {{ selectedEmployee.name }},
+      {{ selectedEmployee.title }}
+      <v-btn color="pink" text @click="snackbar = false"> Close </v-btn>
+    </v-snackbar>
   </div>
-  
 </template>
 
 <script>
-// Static Import
-import json from "../data/employees.json";
+import EmployeesTable from "../components/EmployeesTable";
+import EventTimeline from "../components/EventTimeline";
+import SalesGraph from "../components/SalesGraph";
+import StatisticCard from "../components/StatisticCard";
+
+import employeesData from "../data/employees.json";
+import timelineData from "../data/timeline.json";
+import salesData from "../data/sales.json";
+import statisticsData from "../data/statistics.json";
 
 export default {
-  name: "Dashboard",
-
-  components: {},
-
+  name: "DashboardPage",
+  components: {
+    EmployeesTable,
+    EventTimeline,
+    SalesGraph,
+    StatisticCard,
+  },
   data() {
     return {
-      headers: [
-        {
-          text: "Id",
-          align: "start",
-          sortable: false,
-          value: "id",
-        },
-        { text: "Name", value: "name" },
-        { text: "Job Title", value: "title" },
-        { text: "Salary", value: "salary" },
-      ],
-      employees: json,
+      employees: employeesData,
+      sales: salesData,
+      selectedEmployee: {
+        name: "",
+        title: "",
+      },
+      snackbar: false,
+      statistics: statisticsData,
+      timeline: timelineData,
     };
+  },
+  methods: {
+    setEmployee(event) {
+      this.snackbar = true;
+      this.selectedEmployee.name = event.name;
+      this.selectedEmployee.title = event.title;
+    },
   },
 };
 </script>
